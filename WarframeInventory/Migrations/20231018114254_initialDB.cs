f8e8a4b882cf7bf44f88b23c29e41411ce22b958
Brunopunck49 <brunopunck49@gmail.com>
2023-10-18 09:27:25 -0300

    Migração feita
    Fiz a migração dos dados para o SQLServer. Tive problemas com o certificado, minha solução foi ter colocado "trustServerCertificate=true" em "Database" em "appsettings.json", não é a solução mais sofisticada e não estou satisfeita com ela, mas irá servir para meu aprendizado

---
+++ WarframeInventory/Migrations/20231018114254_initialDB.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WarframeInventory.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    MasteryRank = table.Column<int>(type: "int", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Warframes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Warframes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Warframes");
+        }
+    }
+}
