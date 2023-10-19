02a28824a840ad4fd2c96e1a73e9240d604dab76
Brunopunck49 <brunopunck49@gmail.com>
2023-10-19 08:37:28 -0300

    HttpGet e HttpPost adicionados ao UserModel
    Adicionei classes HttpGet e HttpPost ao UserModel

--- WarframeInventory/Controllers/UserController.cs
+++ WarframeInventory/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WarframeInventory.Models;
+using WarframeInventory.Repositories.Interfaces;
 
 namespace WarframeInventory.Controllers
 {
@@ -8,10 +9,32 @@ namespace WarframeInventory.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<UserModel>> SearchAllUsers()
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository) 
         {
-            return Ok();
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UserModel>>> SearchAllUsers()
+        {
+            List<UserModel> users = await _userRepository.SearchAllUsers();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserModel>> SearchById(int id)
+        {
+            UserModel user = await _userRepository.SearchById(id);
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserModel>> Register([FromBody] UserModel userModel)
+        {
+            UserModel user = await _userRepository.AddUser(userModel);
+            return Ok(user);
         }
     }
 }
